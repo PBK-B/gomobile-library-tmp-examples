f49906f54327b1b68104df781d22579e60eccdbe
PBK-B <pbk-b@PBK6.cn>
2022-07-20 15:34:06 +0800

    feat: add build shell

---
+++  build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+###
+ # @Author: Bin
+ # @Date: 2022-07-20
+ # @FilePath: /goMobileDemoApp/ build.sh
+### 
+
+# build GoMobileDemoApp.xcframework
+gomobile bind -target ios -o ./libs/GoMobileDemoApp.xcframework ./src/
